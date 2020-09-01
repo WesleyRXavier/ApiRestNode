d04fe4608c583b9848ced18086f8b44e53a471b5
wesleyrxavier <wesleyrob9@hotmail.com>
2020-08-31 22:56:18 -0300

    passo 6

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:false}));//apenas dados simples
+app.use(bodyParser.json());//so aceita entrada json
+app.use((req,res,next)=>{
+    res.header('Acces-Control-allow-Origin', '*');//permite acesso de todos os lugares , par restingir e so colocar o servidor no lugar do *
+    res.header('Acces-Control-Header',
+    'Origin, X-Requested-With,Content-Type, Accept, Authorization');
+
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH,DELETE, GET');
+        return res.status(200).send({
+
+        })
+    }
+    next();
+});
+
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
 
--- routes/produtos.js
+++ routes/produtos.js
@@ -1,20 +1,48 @@
 const express = require('express');
 const { Router } = require('express');
 const router = express.Router();
-
+const mysql = require('../mysql').pool;
 
 //RETORNA TODOS OS PRODUTOS
 router.get('/', (req, res, next)=>{
-    res.status(200).send({
-        mensagem:'Usando o GET dentro da rota produtos',
-    });
+mysql.getConnection((error, conn)=>{
+    if(error){return res.status(500).send({error:error});}
+
+    conn.query(
+        'SELECT * FROM produtos;',
+        (error, resultado, fields)=>{
+            conn.release();
+            if(error){return res.status(500).send({error:error});}
+
+            return res.status(200).send({response:resultado});
+
+        }
+    )
+});
+   
 });
 
 // CRIA UM PRODUTO 
 router.post('/',(req,res,next)=>{
-    res.status(201).send({
-        mensagem:'Usando POST na rota Produtos',
+
+    mysql.getConnection((error,conn)=>{
+        if(error){return res.status(500).send({error:error});}
+        conn.query(
+            'INSERT INTO produtos (nome, preco) VALUES(?,?)',
+            [req.body.nome,req.body.preco],
+            (error, resultado, field)=>{
+                conn.release();
+                if(error){return res.status(500).send({error:error});}
+                res.status(201).send({
+                    mensagem:'Produto Inserido com sucesso',
+                    id_produto:resultado.insertId,
+                });
+            }
+
+        )
     });
+
+    
 });
 
 
